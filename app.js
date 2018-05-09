1ef10f81a8999c3203b1f2a40123a068f830b159
RueichiLin-PC\RueichiLin <rueichilin@astrocorp.com.tw>
2018-05-09 16:56:52 +0800

    notifier

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+/** require */
 const moment = require('moment');
-
+const notifier = require('node-notifier');
+const path = require('path');
+/** define */
 const elNow = document.querySelector('.now-time');
 const elAlarm = document.querySelector('.alarm-time');
 let nowTime;
@@ -9,21 +12,32 @@ const onAlarmTextChange = (event) => {
 };
 elAlarm.addEventListener('change', onAlarmTextChange);
 let time = moment();
-
-
 const now = moment(time).format('HH:mm:ss');
 nowTime = now;
 elNow.innerText = now;
-
+// console.log(notifier.supported_notifiers);
 const alarm = moment(time).add(5, 'seconds').format('HH:mm:ss');
 alarmTime = alarm;
 elAlarm.value = alarm;
+const notice = (msg) => {
+  notifier.notify({
+    title: 'Alarm Clock',
+    message: msg,
+    icon: path.join(__dirname, '/clock.png'),
+    /** doesn't work on balloons == */
+    sound: true,
+  });
+};
+let isAlarm = false;
 const check = () => {
   const diff = moment(nowTime, 'HH:mm:ss').diff(moment(alarmTime, 'HH:mm:ss'));
-  if (diff === 0) {
-    alert('wake up!');
+  if (diff === 0 && !isAlarm) {
+    const msg = `It's ${alarmTime}. Wake Up!`;
+    isAlarm = true;
+    notice(msg);
   }
 };
+
 const timer = () => {
   time = moment().format('HH:mm:ss');
   nowTime = time;
